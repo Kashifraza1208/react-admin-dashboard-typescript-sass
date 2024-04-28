cab056ce528834364b8321b481e241544eceba0f
kashif1208 <kashif.raza@techdome.net.in>
2024-04-28 14:21:26 +0530

    admin dashboard

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Customers = lazy(() => import("./pages/Customers"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+const BarCharts = lazy(() => import("./pages/charts/BarCharts"));
+const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
+const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
+
+const NewProduct = lazy(() => import("./pages/management/NewProduct"));
+const ProductManagement = lazy(
+  () => import("./pages/management/ProductManagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/management/TransactionManagement")
+);
+
+const Stopwatch = lazy(() => import("./pages/apps/Stopwatch"));
+const Coupon = lazy(() => import("./pages/apps/Coupon"));
+const Toss = lazy(() => import("./pages/apps/Toss"));
+
+//code spliting means we will not send data in bundle , will be send in chunk means at a time only single component will be loaded on screen. by default all component will not be loaded and it will be possible by lazy loading
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+
+          {/* charts */}
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
+
+          {/* App */}
+
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
+
+          {/* Management */}
+          <Route path="/admin/product/new" element={<NewProduct />} />
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
